dde662a0cfc5e632326e4697db12252a5eadb192
Eric Conlon <ejconlon@gmail.com>
2016-12-15 19:10:35 -0800

    stub csv

--- src/Analyze/Csv.hs
+++ src/Analyze/Csv.hs
@@ -1,4 +1,17 @@
 module Analyze.Csv where
 
-import Data.Csv as C
+import Analyze.RFrame (RFrame(..), RFrameUpdate(..), fromUpdate)
+import Control.Monad.Catch (Exception, MonadThrow (..))
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Csv as C
+import Data.Text (Text)
+import Data.Typeable (Typeable)
 
+data CsvError = CsvError String deriving (Eq, Show, Typeable)
+instance Exception CsvError
+
+readWithHeader :: MonadThrow m => LBS.ByteString -> m (RFrame Text Text)
+readWithHeader = undefined
+
+readWithoutHeader :: MonadThrow m => LBS.ByteString -> m (RFrame Int Text)
+readWithoutHeader = undefined
