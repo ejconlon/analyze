640beec550b92e3b31c4ecb3e4ee8afe71f4939e
Nikita Tchayka <nikitatchayka@gmail.com>
2017-07-30 19:04:40 +0100

    Add easy CSV loading

--- src/Analyze/Csv.hs
+++ src/Analyze/Csv.hs
@@ -1,17 +1,18 @@
 -- | Functions for working with CSV files.
 module Analyze.Csv where
 
-import           Analyze.Conversions  (projectRows)
-import           Analyze.RFrame       (RFrame (..), RFrameUpdate (..), empty, fromUpdate)
-import           Control.Monad.Catch  (Exception, MonadThrow (..))
-import qualified Data.Binary.Builder  as B
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.Csv             as C
-import qualified Data.Csv.Builder     as CB
-import           Data.Text            (Text)
-import           Data.Text.Encoding   (decodeUtf8, encodeUtf8)
-import           Data.Typeable        (Typeable)
-import qualified Data.Vector          as V
+import           Analyze.Conversions        (projectRows)
+import           Analyze.RFrame             (RFrame (..), RFrameUpdate (..), empty, fromUpdate)
+import           Control.Monad.Catch        (Exception, MonadThrow (..))
+import qualified Data.Binary.Builder        as B
+import qualified Data.ByteString.Lazy       as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import qualified Data.Csv                   as C
+import qualified Data.Csv.Builder           as CB
+import           Data.Text                  (Text)
+import           Data.Text.Encoding         (decodeUtf8, encodeUtf8)
+import           Data.Typeable              (Typeable)
+import qualified Data.Vector                as V
 
 -- | Exception to wrap Cassava error strings.
 data CsvError = CsvError String deriving (Eq, Show, Typeable)
@@ -50,3 +51,15 @@ encodeWithHeader (RFrame ks _ vs) =
 encodeWithoutHeader :: RFrame k Text -> LBS.ByteString
 encodeWithoutHeader (RFrame _ _ vs) =
   B.toLazyByteString (foldMap (CB.encodeRecord . (encodeUtf8 <$>)) vs)
+
+
+loadCSVFileWithHeader :: FilePath -> IO (RFrame Text Text)
+loadCSVFileWithHeader fileName = do
+  contents <- readFile fileName
+  decodeWithHeader (LBS8.pack contents)
+  
+loadCSVFileWithoutHeader :: FilePath -> IO (RFrame Int Text)
+loadCSVFileWithoutHeader fileName = do
+  contents <- readFile fileName
+  decodeWithoutHeader (LBS8.pack contents)
+  
